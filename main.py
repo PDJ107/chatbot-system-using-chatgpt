6b3909cbb1ea5daa2b25acd6241db3b3d1ecd2a9
박동주 <cxz541@gmail.com>
2023-05-19 00:08:20 +0900

    Add: 챗봇 API 추가

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 import uvicorn
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+from retriever.es import ES
+from reader.chatgpt import ChatGPT
+
+
+class Request(BaseModel):
+    question: str
+
 
 app = FastAPI()
+retriever = ES()
+reader = ChatGPT()
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.post("/chatbot")
+def get_answer(request: Request):
+    context = '\n---\n'.join(
+        retriever.get_context(
+            query=request.question
+        )
+    )
+    answer = reader.answer_question(
+        question=request.question,
+        context=context
+    )
+    return {'answer': answer}
 
 
 if __name__ == '__main__':
