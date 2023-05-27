a671e2d20c6a2aff2990abdaa6ada57fbee1c78a
박동주 <cxz541@gmail.com>
2023-05-27 20:33:21 +0900

    Fix: get_context index 수정

--- retriever/es.py
+++ retriever/es.py
@@ -1,7 +1,5 @@
 import configparser
-from elasticsearch import Elasticsearch
-from pprint import pprint
-import json
+from elasticsearch import Elasticsearch, exceptions
 
 
 class ES:
@@ -10,12 +8,10 @@ class ES:
         config.read(config_path)
 
         self.es = Elasticsearch(config['ES']['URL'], request_timeout=60 * 1)
-        self.index = config['ES']['INDEX']
 
-        pprint(self.es.info().body)
+        print(self.es.info().body)
 
-    def get_context(self, query, index=None, k=3):
-        index = self.index if index is None else index
+    def get_context(self, query, index, k=2):
         return self.es.search(index=index,
                               query={
                                   'multi_match': {
@@ -31,6 +27,9 @@ class ES:
 
 
 if __name__ == '__main__':
-    retriever = ES('../config.ini')
-    pprint(retriever.get_context('한기대 개교일은?', k=1))
+    config = configparser.ConfigParser()
+    config.read('../config.ini')
+
+    es = ES('../config.ini')
+    print(es.get_context('한기대 개교일', config['RETRIEVER']['INDEX'], k=1))
 
