56e1dea86917508e3a49603c226370eeb5277d86
박동주 <cxz541@gmail.com>
2023-05-28 00:02:20 +0900

    Add: chat history 저장 기능 추가

--- retriever/es.py
+++ retriever/es.py
@@ -8,18 +8,52 @@ class ES:
         config.read(config_path)
 
         self.es = Elasticsearch(config['ES']['URL'], request_timeout=60 * 1)
-
-        print(self.es.info().body)
+        self.init_messages = eval(config['OPENAI']['INIT_MESSAGE'])
 
     def get_context(self, query, index, k=2):
-        return self.es.search(index=index,
-                              query={
-                                  'multi_match': {
-                                      'query': query,
-                                      'fields': ['*']
-                                  }
-                              },
-                              size=k).body
+        query = {
+            "multi_match": {
+              "query": query,
+            }
+        }
+        response = self.es.search(index=index,
+                                  query=query,
+                                  size=k).body
+
+        if response['_shards']['total'] == 0:
+            return []
+        return [context['_source']['contents'] for context in response['hits']['hits']]
+
+    def get_history(self, user_id):
+
+        try:
+            return self.es.get(index='messages', id=user_id)['_source']['messages']
+
+        except exceptions.NotFoundError:
+
+            print("Not Found")
+            return self.init_history(user_id)
+
+    def update_history(self, user_id, messages):
+        doc = {
+            'messages': messages
+        }
+
+        try:
+            self.es.update(index='messages', id=user_id, doc=doc)
+
+        except exceptions.NotFoundError:
+
+            print("Not Found")
+            self.es.index(index='messages', id=user_id, document=doc)
+
+    def init_history(self, user_id):
+        init_doc = {
+            'messages': self.init_messages
+        }
+
+        self.es.index(index='messages', id=user_id, document=init_doc)
+        return init_doc['messages']
 
     def input_data(self, contexts, index):
         for context in contexts:
